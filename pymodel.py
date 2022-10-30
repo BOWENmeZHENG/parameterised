d94fdb57e415c7bab951416d52f66281f5780001
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-10-30 12:28:12 -0700

    first spoke

--- pymodel.py
+++ pymodel.py
@@ -16,13 +16,22 @@ from connectorBehavior import *
 r_out = 0.3
 r_in = 0.2
 width = 0.1
+spoke_width = 0.04
 meshsize = 0.02
 r_depth = 0.02
 r_pressure = 0.1
-search_point = (0.0, r_out, width/2)
 
+# Derived values
+search_point_whole = (0.0, r_out, width / 2)
+search_point_lateral = (0.0, r_out, width / 2)
+search_point_extrusion = (0.0, r_in, width)
+search_point_outer_edge = (0.0, r_out, width)
+spoke_start = (r_out + r_in) / 2
+
+
+# Define wheel geometry
 mymodel = mdb.models['Model-1']
-mymodel.ConstrainedSketch(name='__profile__', sheetSize=r_out*2)
+mymodel.ConstrainedSketch(name='__profile__', sheetSize=r_out * 2)
 mymodel.sketches['__profile__'].CircleByCenterPerimeter(center=(0.0, 0.0), point1=(r_out, 0.0))
 mymodel.sketches['__profile__'].CircleByCenterPerimeter(center=(0.0, 0.0), point1=(r_in, 0.0))
 mymodel.Part(dimensionality=THREE_D, name='Part-1', type=DEFORMABLE_BODY)
@@ -31,21 +40,33 @@ mypart = mdb.models['Model-1'].parts['Part-1']
 mypart.BaseSolidExtrude(depth=width, sketch=mymodel.sketches['__profile__'])
 del mymodel.sketches['__profile__']
 
+# Define spoke geometry
+face_base = mypart.faces.findAt((search_point_extrusion,), )[0]
+edge_extrusion = mypart.edges.findAt((search_point_outer_edge,), )[0]
+mymodel.ConstrainedSketch(gridSpacing=0.04, name='__profile__', sheetSize=1.7,
+                          transform=mypart.MakeSketchTransform(
+                              sketchPlane=face_base, sketchPlaneSide=SIDE1, sketchUpEdge=edge_extrusion,
+                              sketchOrientation=RIGHT, origin=(0.0, 0.0, width)))
+mypart.projectReferencesOntoSketch(filter=COPLANAR_EDGES, sketch=mymodel.sketches['__profile__'])
+mymodel.sketches['__profile__'].rectangle(point1=(-spoke_start, -spoke_width/2), point2=(spoke_start, spoke_width/2))
+mypart.SolidExtrude(depth=width, flipExtrudeDirection=ON, sketch=mymodel.sketches['__profile__'],
+                    sketchOrientation=RIGHT, sketchPlane=face_base, sketchPlaneSide=SIDE1, sketchUpEdge=edge_extrusion)
+del mymodel.sketches['__profile__']
+
 mypart.seedPart(deviationFactor=0.1, minSizeFactor=0.1, size=meshsize)
-mypart.setMeshControls(elemShape=TET, regions=mypart.cells.findAt((search_point,), ), technique=FREE)
+mypart.setMeshControls(elemShape=TET, regions=mypart.cells.findAt((search_point_whole,), ), technique=FREE)
 mypart.setElementType(elemTypes=(ElemType(elemCode=C3D8R, elemLibrary=STANDARD),
                                  ElemType(elemCode=C3D6, elemLibrary=STANDARD),
                                  ElemType(elemCode=C3D4, elemLibrary=STANDARD,
                                           secondOrderAccuracy=OFF, distortionControl=DEFAULT)),
-                      regions=(mypart.cells.findAt(((0.0, r_out, width/2),), ),))
+                      regions=(mypart.cells.findAt(((0.0, r_out, width / 2),), ),))
 mypart.generateMesh()
 
-# get face by coordinates
-
-mypart.Set(faces=mypart.faces.findAt((search_point,), ), name='face_big')
+# get nodes for loading
+mypart.Set(faces=mypart.faces.findAt((search_point_lateral,), ), name='face_big')
 face_big = mypart.sets['face_big'].faces[0]
 mypart.Set(nodes=face_big.getNodes(), name='face_nodes')
 face_big_nodes = mypart.sets['face_nodes'].nodes
-mypart.Set(nodes=face_big_nodes.getByBoundingCylinder(center1=(0.0, r_out-r_depth, width/2),
-                                                      center2=(0.0, r_out+r_depth, width/2),
+mypart.Set(nodes=face_big_nodes.getByBoundingCylinder(center1=(0.0, r_out - r_depth, width / 2),
+                                                      center2=(0.0, r_out + r_depth, width / 2),
                                                       radius=r_pressure), name='nodes_load')
