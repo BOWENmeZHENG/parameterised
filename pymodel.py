909d2ac09567ddabdc924d68f78fdd28f5580a11
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-10-31 15:02:51 -0700

    remove imports

--- pymodel.py
+++ pymodel.py
@@ -1,16 +1,4 @@
-from part import *
-from material import *
-from section import *
-from assembly import *
-from step import *
-from interaction import *
-from load import *
-from mesh import *
-from optimization import *
-from job import *
-from sketch import *
 from visualization import *
-from connectorBehavior import *
 
 import abaqus_utils as ut
 
