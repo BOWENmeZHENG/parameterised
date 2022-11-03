73b701fd648ec002a04ac8c0008a9b3679a52e08
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-11-03 10:51:53 -0700

    main

--- automate.py
+++ automate.py
@@ -4,19 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# run
-filename = w.write_pymodel(r_out=0.3, r_in=0.2, width=0.1, spoke_width=0.04, num_spokes=6)
-os.system(f"abaqus cae noGUI={filename}")
-os.remove(filename + '.py')
+def run_model(r_out, r_in, width, spoke_width, num_spokes, vis=False):
+    filename = w.write_pymodel(r_out=r_out, r_in=r_in, width=width, spoke_width=spoke_width, num_spokes=num_spokes)
+    os.system(f"abaqus cae noGUI={filename}")
+    os.remove(filename + '.py')
 
-# Visualize
-# nodes = pd.read_csv(f"{filename}_nodes.csv")
-# x, y, z = nodes.x, nodes.y, nodes.z
-# fig = plt.figure(figsize=(8, 6))
-# ax = fig.add_subplot(projection='3d')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z')
-# sca = ax.scatter(x, y, z, c=nodes.s11)
-# plt.colorbar(sca, pad=0.1)
-# plt.show()
+    # Visualize
+    if vis:
+        nodes = pd.read_csv(f"{filename}_nodes.csv")
+        x, y, z = nodes.x, nodes.y, nodes.z
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(projection='3d')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        sca = ax.scatter(x, y, z, c=nodes.s11)
+        plt.colorbar(sca, pad=0.1)
+        plt.show()
---
+++ main.py
@@ -0,0 +1,4 @@
+from automate import run_model
+
+
+run_model(r_out=0.3, r_in=0.2, width=0.1, spoke_width=0.04, num_spokes=4, vis=True)
