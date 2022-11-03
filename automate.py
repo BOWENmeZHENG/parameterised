10899a62090b09d7d4ccafc24b3e7a17faae3d9a
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-11-03 10:46:26 -0700

    delete py script after run

--- automate.py
+++ automate.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 
 
 # run
-filename = w.write_pymodel(r_out=0.3, r_in=0.2, width=0.1, spoke_width=0.04, num_spokes=3)
+filename = w.write_pymodel(r_out=0.3, r_in=0.2, width=0.1, spoke_width=0.04, num_spokes=6)
 os.system(f"abaqus cae noGUI={filename}")
+os.remove(filename + '.py')
 
 # Visualize
-nodes = pd.read_csv(f"{filename}_nodes.csv")
-x, y, z = nodes.x, nodes.y, nodes.z
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(projection='3d')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-sca = ax.scatter(x, y, z, c=nodes.s11)
-plt.colorbar(sca, pad=0.1)
-plt.show()
+# nodes = pd.read_csv(f"{filename}_nodes.csv")
+# x, y, z = nodes.x, nodes.y, nodes.z
+# fig = plt.figure(figsize=(8, 6))
+# ax = fig.add_subplot(projection='3d')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+# sca = ax.scatter(x, y, z, c=nodes.s11)
+# plt.colorbar(sca, pad=0.1)
+# plt.show()
